5f6279bc16e82f2d7564cf7d2064848a49a449e8
Yousuke Nakao <yoosukenakao@thewaggle.co.jp>
2018-02-08 21:02:38 +0900

    MeBot list make

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import BodyText from './src/elements/BodyText';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello</Text>
-        <BodyText />
+        <View style={styles.appber}>
+          <View>
+            <Text style={styles.appberTitle}>MEBOT</Text>
+          </View>
+        </View>
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2018/02/21</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2018/02/21</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2018/02/21</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2018/02/21</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のアイテム</Text>
+            <Text style={styles.memoDate}>2018/02/21</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoAddButton}>
+          <Text style={styles.memoAddText}>+</Text>
+        </View>
       </View>
     );
   }
@@ -17,8 +49,66 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF5',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  appber: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    backgroundColor: '#369',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+  memoList: {
+    width: '100%',
+    flex: 1,
+    paddingTop: 78,
+  },
+  memoListItem: {
+    backgroundColor: '#FFF',
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+  appberTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  memoAddText: {
+    fontSize: 24,
+    color: '#fff',
   },
 });
